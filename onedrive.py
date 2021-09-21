c4ce8dcc0ad26329e34e759f490280272f4f6af9
KrishnaCD93 <krishna.duvvuri93@gmail.com>
2021-09-21 00:14:35 -0600

    update

--- onedrive.py
+++ onedrive.py
@@ -3,19 +3,23 @@ import pandas as pd
 
 
 class OneDriveDataset:
-    def __init__(self, name=None):
+    def __init__(self, onedrive_link, name=None, excel=None):
+        self.onedrive_link = onedrive_link
         self.name = name
+        self.excel = excel
+
     
-    def onedrive_directdownload (self, onedrive_link):
-        data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+    def onedrive_directdownload (self):#, onedrive_link, excel=None):
+        data_bytes64 = base64.b64encode(bytes(self.onedrive_link, 'utf-8'))
         data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
         resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
-        df = pd.read_csv(resultUrl)
+        if self.excel is not None: df = pd.read_excel(resultUrl)
+        else: df = pd.read_csv(resultUrl)
         self.df = df
         return df
 
     def __getitem__(self, i):
-        return self.df
+        return self.df[i]
 
     def __len__(self):
         return 1
