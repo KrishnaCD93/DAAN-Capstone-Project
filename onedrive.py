39cdb96e0d6205d780d992715dbfd5d73cd625eb
KrishnaCD93 <krishna.duvvuri93@gmail.com>
2021-09-26 13:41:02 -0600

    update onedrive.py to include more excel options

--- onedrive.py
+++ onedrive.py
@@ -5,10 +5,12 @@ import pandas as pd
 class OneDriveDataset:
     """Download one drive data via the share link. Specify if the file is excel format by passing excel=True."""
 
-    def __init__(self, onedrive_link, name=None, excel=None):
+    def __init__(self, onedrive_link, name=None, excel=None, sheet=0, header=0):
         self.onedrive_link = onedrive_link
         self.name = name
         self.excel = excel
+        self.sheet = sheet
+        self.header = header
 
     def onedrive_directdownload(self):
         """Download and return dataframe"""
@@ -20,7 +22,7 @@ class OneDriveDataset:
             f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
         )
         if self.excel is not None:
-            df = pd.read_excel(resultUrl)
+                df = pd.read_excel(resultUrl, sheet_name=self.sheet, header=self.header)
         else:
             df = pd.read_csv(resultUrl)
         self.df = df
